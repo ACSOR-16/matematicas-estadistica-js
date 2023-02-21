78b9c9124de08ed879ed754347e196975b6620f5
oscar <oscar.aguirre.tarazona@gmail.com>
2023-02-21 08:37:57 -0500

    funcion para calcular area y circunferencia del circulo

--- math/main.js
+++ math/main.js
@@ -1,7 +1,7 @@
 
 //FUNCION PARA EL CALCULAR EL CUADRADO
-function calcularCuacrado () {
-  return console.log({
+function calcularCuacrado (ladoCuadrado) {
+  return ({
     perimetroCuadrado: ladoCuadrado * 4,
     areaCuadrado: Math.pow(ladoCuadrado, 2),
   });
@@ -11,10 +11,18 @@ calcularCuacrado();
 
 // FUNCION PARA CALCULAR EL TRIANGULO
 function calcularTriangulo(ladoTriangulo1, ladoTriangulo2, ladoTriangulo3, base, altura)  {
-  return console.log({
+  return({
     perimetroTriangulo: ladoTriangulo1 + ladoTriangulo2 + ladoTriangulo3,
     areaTriangulo: (base * altura) / 2.
   })
 }
 
 calcularTriangulo();
+
+function calcularCirculo(radioCirculo){
+  const diametroCirulo = radioCirculo * 2;
+  return({
+    circunferencia: diametroCirulo * Math.PI,
+    areaCirculo: (Math.pow(radioCirculo, 2)) * Math.PI,
+  })
+}
