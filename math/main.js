8c8f5a7c5c4230b2b5a28805d0689291a62ff9c8
oscar <oscar.aguirre.tarazona@gmail.com>
2023-02-21 09:52:38 -0500

    altura de triangulo isoseles y escaleno

--- math/main.js
+++ math/main.js
@@ -19,6 +19,27 @@ function calcularTriangulo(ladoTriangulo1, ladoTriangulo2, ladoTriangulo3, base,
 
 calcularTriangulo();
 
+function calcularTrianguloIsoseles (ladoIso, base) {
+  if (ladoIso == base) {
+    console.warn("Este no es un Triangulo Isoseles")
+  } else {
+    return({
+      alturaIsoseles: Math.sqrt(Math.pow(ladoIso, 2)-(Math.pow(base,2) / 4)),
+    })
+  }  
+}
+
+function calcularTrianguloEscaleno(primerLado, segundoLado, tercerLado) {
+  const semiPerimetro = (primerLado + segundoLado + tercerLado) / 2;
+  const areaTriangulo = Math.sqrt(semiPerimetro * (semiPerimetro - primerLado) * (semiPerimetro - segundoLado) * (semiPerimetro - tercerLado));
+  
+  return({
+    alturaPrimerLado: areaTriangulo * 2 - primerLado,
+    alturaSegundLado: areaTriangulo * 2 - segundoLado,
+    alturaTercerLado: areaTriangulo * 2 - tercerLado,
+  })
+}
+
 function calcularCirculo(radioCirculo){
   const diametroCirulo = radioCirculo * 2;
   return({
