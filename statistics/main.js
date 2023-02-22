1e4909ba1256a23c0422d15121a09ad2d2e9e9a0
oscar <oscar.aguirre.tarazona@gmail.com>
2023-02-22 16:58:36 -0500

    calculo demediana de una lista impar

--- statistics/main.js
+++ statistics/main.js
@@ -13,6 +13,31 @@ mediaAritmetica([1, 2, 3, 4, 5, 6]);
 
 function mediaAritmeticaReduce(array) {
   initialValue = 0;
+  
   const sumWithInitialValue = array.reduce( (acucumulator, currentValue) => acucumulator + currentValue, initialValue);
-  return sumWithInitialValue / array.length;
+  
+  const promedio = sumWithInitialValue / array.length;
+  return promedio;
+}
+
+// CALCULO DE MEDIANA 
+function esPar(array) {
+  return !(array.length % 2);
+}
+
+function esImpar(array) {
+  return array.length % 2;
+}
+
+function calcularMediana(array) {
+  const arrayEsPar = esImpar(array)
+  
+  if (arrayEsPar) {
+      
+  } else {
+    const indexMitadListaImpar = Math.floor(array.length / 2);
+    const medianaListaImpar = array[indexMitadListaImpar];
+    
+    return medianaListaImpar;
+  }
 }
