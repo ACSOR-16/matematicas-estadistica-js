0d8f100613847f97beb999f398b18f1141d4fbeb
oscar <oscar.aguirre.tarazona@gmail.com>
2023-02-24 10:05:20 -0500

    proyeccion salarial de persona

--- analisisSalarial/analisis.js
+++ analisisSalarial/analisis.js
@@ -15,3 +15,22 @@ function medianaPorPersona(nombreDePersona) {
   const medianaSalarial = StatdMath.calcularMediana(salariosTotales);
   console.log(medianaSalarial);
 }
+
+function proyeccioPersona(nombreDePersona) {
+  const trabajos = encontrarPersona(nombreDePersona).trabajos;
+  const salariosTotales = trabajos.map( (elemento) => elemento.salario );
+  
+  let creciemintoPorc = [];
+
+  for (let index = 1; index < salariosTotales.length; index++) {
+    const salarioActual = salariosTotales[index];
+    const salarioAnterior = salariosTotales[index - 1];
+    const aumento = salarioActual - salarioAnterior;
+    const porcentajeSueldos = aumento / salarioAnterior;
+    creciemintoPorc.push(porcentajeSueldos);
+  }
+  const medianaPorcentualSalaria = StatdMath.calcularMediana(creciemintoPorc);
+  const ultimoSalario = salariosTotales[salariosTotales.length - 1];
+  const salarioProyectado = ultimoSalario + (medianaPorcentualSalaria * ultimoSalario);
+  return salarioProyectado;
+}
